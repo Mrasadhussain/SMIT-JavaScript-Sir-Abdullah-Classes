32b5bcd6556d2acaac61f74c0c5845dd0dfacedb
Mrasadhussain <asadsmit11@gmail.com>
2024-03-05 20:43:39 +0500

    index file ceated

---
+++ 1-Class-Introduction/app.js
@@ -0,0 +1,36 @@
+alert('Hello World')
+
+
+console.log("Hello world");
+
+
+// string "asdljkads3423423 true"
+// number 12345254
+// boolean true || false
+// object
+
+
+
+var $_userName123 = "asad";
+console.log($_userName123);
+
+
+var userName = "asad";
+console.log(userName);
+
+
+var firstName = "asad";
+var lastName = "hussain";
+var age = 20;
+var isAvailable = true;
+// concatinate
+console.log(typeof(isAvailable));
+console.log(firstName + " " + lastName);
+console.log(12 + 12);
+
+
+var firstName = 'asad';
+var lastName = 'hussain';
+var age = 20;
+
+console.log('my name is ' + firstName + " " + lastName + ' and my age is ' + age);
